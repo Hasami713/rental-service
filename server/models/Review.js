9ca74f09cb3589e9148e02801fe5d0ff7bba1d95
Hasami713 <amir.khasanov713@gmail.com>
2025-06-13 01:31:31 +0300

    lab4

--- server/models/Review.js
+++ server/models/Review.js
@@ -4,18 +4,20 @@ import User from "./User.js";
 import Offer from "./Offer.js";
 
 const Review = sequelize.define('Review', {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-  },
   text: {
-    type: DataTypes.TEXT,
+    type: DataTypes.STRING,
     allowNull: false,
+    validate: { len: [5, 1024] }
+  },
+  publishDate: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW
   },
   rating: {
     type: DataTypes.INTEGER,
     allowNull: false,
+    validate: { min: 1, max: 5 }
   }
 }, {
   tableName: 'reviews',
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
-import { registration } from "../controllers/userController.js";
+import {checkAuth, login, logout, registration} from "../controllers/userController.js";
 import upload from "../middleware/upload.js";
+import {authenticateToken} from "../middleware/authModdleware.js";
 
 const router = new Router();
 
 router.post('/register', upload.single('avatar'), registration);
-
+router.post('/login', login);
+router.get('/login', authenticateToken, checkAuth);
+router.delete('/logout', logout);
 export default router;
