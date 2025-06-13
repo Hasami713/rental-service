d9003611bbc01fc27aefcf755ea4eebadc3557b1
Hasami713 <amir.khasanov713@gmail.com>
2025-06-13 11:33:07 +0300

    bug fix

--- server/models/Offer.js
+++ server/models/Offer.js
@@ -1,27 +1,89 @@
-import { DataTypes } from 'sequelize';
+import {DataTypes} from 'sequelize';
 import sequelize from '../config/database.js';
 
 const Offer = sequelize.define('Offer', {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-  },
-  title: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  price: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  type: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  }
-}, {
-  tableName: 'offers',
-  timestamps: false,
-});
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {len: [10, 100]}
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {len: [20, 1024]}
+        },
+        publishDate: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+        },
+        city: {
+            type: DataTypes.ENUM('Paris', 'Cologne', 'Brussels', 'Amsterdam', 'Hamburg', 'Dusseldorf'),
+            allowNull: false
+        },
+        previewImage: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        photos: {
+            type: DataTypes.ARRAY(DataTypes.STRING),
+            allowNull: false,
+
+
+        },
+        isPremium: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false
+        },
+        isFavorite: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false
+        },
+        rating: {
+            type: DataTypes.DECIMAL(2, 1),
+            allowNull: false,
+            validate: {min: 1, max: 5}
+        },
+        type: {
+            type: DataTypes.ENUM('apartment', 'house', 'room', 'hotel'),
+            allowNull: false
+        },
+        rooms: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {min: 1, max: 8}
+        },
+        guests: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {min: 1, max: 10}
+        },
+        price: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {min: 100, max: 100000}
+        },
+        features: {
+            type: DataTypes.ARRAY(DataTypes.ENUM('Breakfast', 'Air conditioning', 'Laptop friendly workspace', 'Baby seat', 'Washer', 'Towels', 'Fridge')),
+            allowNull: false
+        },
+        commentsCount: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0
+        },
+        latitude: {
+            type: DataTypes.FLOAT,
+            allowNull: false
+        },
+        longitude: {
+            type: DataTypes.FLOAT,
+            allowNull: false
+        }
+    }, {
+        sequelize,
+        modelName: 'Offer',
+        tableName: 'offers'
+    }
+);
 
 export default Offer;
